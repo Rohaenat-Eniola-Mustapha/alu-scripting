f3230c1f991c1bf7ff18d70a6f8b17c2caf0e1c0
ea7a024f03bf <r.mustapha@alustudent.com>
2023-09-04 10:25:43 -0700

    first task

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,21 @@
 #!/usr/bin/env ruby
-school
+
+# Check if there is a single command-line argument
+if ARGV.length != 1
+  puts "Usage: #{$0} <string>"
+  exit 1
+end
+
+# Get the argument from the command line
+input_string = ARGV[0]
+
+# Define the regular expression
+pattern = /School/
+
+# Check if the input string matches the regular expression
+if input_string =~ pattern
+  puts input_string
+else
+  puts "$"
+end
 
