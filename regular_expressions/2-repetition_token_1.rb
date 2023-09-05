67907e4e7775b0e5bb5ee277c4f4ab6418e85c42
ea7a024f03bf <r.mustapha@alustudent.com>
2023-09-04 23:50:58 -0700

    third task

---
+++ regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+# Check if there is a single command-line argument
+if ARGV.length != 1
+  puts "Usage: #{$0} <string>"
+  exit 1
+end
+
+# Get the argument from the command line
+input_string = ARGV[0]
+
+# Define the regular expression pattern
+pattern = /hb?tn/
+
+# Initialize an empty result string to store matches
+result = ""
+
+# Use the scan method to find all matches and concatenate them
+input_string.scan(pattern) { |match| result += match }
+
+# Print the result
+puts result
+
